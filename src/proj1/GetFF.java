5020af2c08b3b03e6c64aa68f4508aa57e5930de
temmie <83168581+temmie-v@users.noreply.github.com>
2021-05-24 23:06:06 +0900

    fix indent

--- src/proj1/GetFF.java
+++ src/proj1/GetFF.java
@@ -16,22 +16,22 @@ import twitter4j.TwitterFactory;
 
 public final class GetFF {
     
-	public static void main(String[] args) throws TwitterException, IOException {
+    public static void main(String[] args) throws TwitterException, IOException {
 
-		Calendar time = Calendar.getInstance();
+        Calendar time = Calendar.getInstance();
         String path = "filepath/log.csv";
-		File file = new File(path);
+        File file = new File(path);
         PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"Shift-JIS")));
 
-	    long myid = user_id;
-		
+        long myid = user_id;
+
         pw.write(time.getTime() + ",user_name,@screen_name\n");
         pw.write("\n\nFollowers\n");
         pw.write("ID,@Screen Name,Name,Following,Followers,Protected,Tweets,Likes,CreatedAt,location,description\n");
         System.out.println("Listing followers.");
-		listFers(myid, pw);
+        listFers(myid, pw);
 		
-		pw.write("\n\n\nFollowings\n");
+        pw.write("\n\n\nFollowings\n");
         pw.write("ID,@Screen Name,Name,Following,Followers,Protected,Tweets,Likes,CreatedAt,location,description\n");
         System.out.println("\nListing followings.");
         listFings(myid, pw);
@@ -39,7 +39,7 @@ public final class GetFF {
         pw.close();
         System.out.println("\nSuccessfully finished listing. -> log.csv");
         System.exit(0);
-	}
+    }
 
 
 
@@ -50,16 +50,16 @@ public final class GetFF {
             IDs ids;
 
             do {
-            	ids = twitter.getFollowersIDs(myid, cursor);
-            	for (long id : ids.getIDs()) {
-            		String unit = "";
-            		unit += Long.toString(id);
-            		unit += ",@";
-            		twitter4j.User u = twitter.showUser(id);
-            		unit += makeUnit(u);
-            		unit += "\n";
+                ids = twitter.getFollowersIDs(myid, cursor);
+                for (long id : ids.getIDs()) {
+                    String unit = "";
+                    unit += Long.toString(id);
+                    unit += ",@";
+                    twitter4j.User u = twitter.showUser(id);
+                    unit += makeUnit(u);
+                    unit += "\n";
                     fw.write(unit);
-            		System.out.println(unit);
+                    System.out.println(unit);
                 }
             } while ((cursor = ids.getNextCursor()) != 0);
             fw.write("...total," + twitter.showUser(myid).getFollowersCount());
@@ -78,16 +78,16 @@ public final class GetFF {
             long cursor = -1;
             IDs ids;
             do {
-            	ids = twitter.getFollowersIDs(myid, cursor);
-            	for (long id : ids.getIDs()) {
-               		String unit = "";
-            		unit += Long.toString(id);
-            		unit += ",@";
-            		twitter4j.User u = twitter.showUser(id);
+                ids = twitter.getFollowersIDs(myid, cursor);
+                for (long id : ids.getIDs()) {
+                    String unit = "";
+                    unit += Long.toString(id);
+                    unit += ",@";
+                    twitter4j.User u = twitter.showUser(id);
 
-            		unit += makeUnit(u);
+                    unit += makeUnit(u);
 
-            		unit += "\n";
+                    unit += "\n";
 
                     fw.write(unit);
             		System.out.println(unit);
@@ -104,31 +104,31 @@ public final class GetFF {
     }
 
     private static String makeUnit(twitter4j.User u) {
-    	String ret = "";
-		//ID,@Screen Name,Name,Following,Followers,Protected,Tweets,Likes,CreatedAt,location,description
+        String ret = "";
+        //ID,@Screen Name,Name,Following,Followers,Protected,Tweets,Likes,CreatedAt,location,description
 
-    	ret += u.getScreenName();
-		ret += ",";
-		ret += u.getName().replace(",", "");
-		ret += ",";
-		ret += u.getFriendsCount();
-		ret += ",";
-		ret += u.getFollowersCount();
-		ret += ",";
-		if (u.isProtected()) {
-			ret += "Protected";
-		}
-		ret += ",";
-		ret += u.getStatusesCount();
-		ret += ",";
-		ret += u.getFavouritesCount();
-		ret += ",";
-		ret += u.getCreatedAt();
-		ret += ",";
-		ret += u.getLocation().replaceAll(",", "");
-		ret += ",";
-		ret += u.getDescription().replaceAll("\n", "").replaceAll(",", "");
+        ret += u.getScreenName();
+        ret += ",";
+        ret += u.getName().replace(",", "");
+        ret += ",";
+        ret += u.getFriendsCount();
+        ret += ",";
+        ret += u.getFollowersCount();
+        ret += ",";
+        if (u.isProtected()) {
+            ret += "Protected";
+        }
+        ret += ",";
+        ret += u.getStatusesCount();
+        ret += ",";
+        ret += u.getFavouritesCount();
+        ret += ",";
+        ret += u.getCreatedAt();
+        ret += ",";
+        ret += u.getLocation().replaceAll(",", "");
+        ret += ",";
+        ret += u.getDescription().replaceAll("\n", "").replaceAll(",", "");
 
-		return ret;
+        return ret;
     }
 }
