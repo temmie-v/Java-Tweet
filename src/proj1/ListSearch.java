f31fbe67215f198c8b5dac8c2eec2cf71a3c2408
temmie <83168581+temmie-v@users.noreply.github.com>
2021-05-24 23:10:11 +0900

    fix indent

--- src/proj1/ListSearch.java
+++ src/proj1/ListSearch.java
@@ -18,12 +18,11 @@ public final class ListSearch {
 
     public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
 
-    	try {
-    		long lis = list_id;
+        try {
+            long lis = list_id;
             String path = "filepath/list.csv";
-    		File file = new File(path);
-            PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter
-            						(new FileOutputStream(file),"Shift-JIS")));
+            File file = new File(path);
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"Shift-JIS")));
             
             pw.write("ID,@Screen Name,Name,Following,Followers,Protected,Tweets,Likes,CreatedAt,location,description\n");
             System.out.println("Listing list : " + lis);
@@ -35,42 +34,40 @@ public final class ListSearch {
             do {
                 usres = twitter.getUserListMembers(lis, cursor);
                 for (User list : usres) {
-               		String unit = "";
-            		unit += Long.toString(list.getId());
-            		unit += ",@";
-  	    	    	unit += list.getScreenName();
-  	    	    	unit += ",";
-	    			unit += list.getName().replace(",", "");
-	    			unit += ",";
-	    			unit += list.getFriendsCount();
-	    			unit += ",";
-	    			unit += list.getFollowersCount();
-	    			unit += ",";
-	    			if (list.isProtected()) {
-	    				unit += "Protected";
-	    			}
-	    			unit += ",";
-	    			unit += list.getStatusesCount();
-	    			unit += ",";
-	    			unit += list.getFavouritesCount();
-	    			unit += ",";
-	    			unit += list.getCreatedAt();
-	    			unit += ",";
-	    			unit += list.getLocation().replaceAll(",", "");
-	    			unit += ",";
-	    			unit += list.getDescription().replaceAll("\n", "").replaceAll(",", "");
+                    String unit = "";
+                    unit += Long.toString(list.getId());
+                    unit += ",@";
+                    unit += list.getScreenName();
+                    unit += ",";
+                    unit += list.getName().replace(",", "");
+                    unit += ",";
+                    unit += list.getFriendsCount();
+                    unit += ",";
+                    unit += list.getFollowersCount();
+                    unit += ",";
+                    if (list.isProtected()) {
+                        unit += "Protected";
+                    }
+                    unit += ",";
+                    unit += list.getStatusesCount();
+                    unit += ",";
+                    unit += list.getFavouritesCount();
+                    unit += ",";
+                    unit += list.getCreatedAt();
+                    unit += ",";
+                    unit += list.getLocation().replaceAll(",", "");
+                    unit += ",";
+                    unit += list.getDescription().replaceAll("\n", "").replaceAll(",", "");
 
-	    			unit += "\n";
-	    			pw.write(unit);
-
+                    unit += "\n";
+                    pw.write(unit);
                     System.out.println(unit);
                 }
             } while ((cursor = usres.getNextCursor()) != 0);
             pw.close();
             System.out.println("\nSuccessfully finished listing. ->Desktop\\list.csv");
             System.exit(0);
-
-    	} catch (TwitterException te) {
+        } catch (TwitterException te) {
             te.printStackTrace();
             System.out.println("Failed to get list members: " + te.getMessage());
             System.exit(-1);
